59d01063d20092b031a48d7545ed296b19ebfc75
Frederic Marcel Wahl <fwahl@student.heilbronn.de>
2024-03-01 19:20:32 +0100

    adding pwd

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 16:47:26 by fwahl             #+#    #+#             */
-/*   Updated: 2024/03/01 17:14:21 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/03/01 19:19:10 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ms_execute_commands(t_token *tokens)
 	if (!(ft_strcmp("echo",tokens->token)))
 		ms_echo(tokens);
 	else if (!(ft_strcmp("cd",tokens->token)))
-		ms_cd();
+		ms_cd(tokens);
 	else if (!(ft_strcmp("pwd",tokens->token)))
 		ms_pwd();
 	else if (!(ft_strcmp("export",tokens->token)))
